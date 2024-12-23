b0610aa45c97155e3b810fc3f9fb193ceeaa597e
yitzhak <matias6@gmail.com>
2024-12-22 20:15:07 -0400

    tailwind was added

--- src/components/ContactFilter.vue
+++ src/components/ContactFilter.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <input v-model="name" placeholder="Filter by name" />
-    <input v-model="email" placeholder="Filter by email" />
-    <button @click="applyFilter">Filter</button>
+  <div class="mb-4 flex space-x-4">
+    <input v-model="name" placeholder="Filter by name" class="border border-gray-300 px-4 py-2 rounded w-1/2" />
+    <input v-model="email" placeholder="Filter by email" class="border border-gray-300 px-4 py-2 rounded w-1/2" />
+    <button @click="applyFilter" class="bg-blue-500 text-white px-4 py-2 rounded">Filter</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      name: '',
-      email: '',
-    };
+    return { name: '', email: '' };
   },
   methods: {
     applyFilter() {
